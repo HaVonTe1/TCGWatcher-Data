99f7c5b7b3a338c59c335df80324981853b77d89
Dirk Kutzer <dirk.kutzer@bdr.de>
2025-11-28 14:08:37 +0100

    chore: update Maven wrapper to version 3.3.2, java 25 and Spring Boot 4 and removed Spring Shell

--- src/main/java/de/dktutzer/tcgwatcher/data/config/TcgWatcherDataConfiguration.java
+++ src/main/java/de/dktutzer/tcgwatcher/data/config/TcgWatcherDataConfiguration.java
@@ -1,43 +1,46 @@
 package de.dktutzer.tcgwatcher.data.config;
 
-
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 
 @Configuration
-@Slf4j
 public class TcgWatcherDataConfiguration {
+
+  private static final Logger log = LoggerFactory.getLogger(TcgWatcherDataConfiguration.class);
 
   private final RestTemplateBuilder restTemplateBuilder;
 
   public TcgWatcherDataConfiguration(RestTemplateBuilder restTemplateBuilder) {
     this.restTemplateBuilder = restTemplateBuilder;
   }
+
   @Bean
   public RestTemplate restTemplate() {
-    return restTemplateBuilder.errorHandler(new ResponseErrorHandler() {
-      @Override
-      public boolean hasError(ClientHttpResponse response) throws IOException {
-        var statusCode = response.getStatusCode();
-        return !statusCode.is2xxSuccessful();
-      }
+    return restTemplateBuilder
+        .errorHandler(
+            new ResponseErrorHandler() {
+              @Override
+              public boolean hasError(ClientHttpResponse response) throws IOException {
+                return response.getStatusCode().isError();
+              }
 
-      @Override
-      public void handleError(ClientHttpResponse response) throws IOException {
-        var statusCode = response.getStatusCode();
+              @Override
+              public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
+                log.error("RestTemplate error: {}", response.getStatusCode());
+                throw new HttpStatusCodeException(response.getStatusCode()) {};
+              }
 
-        log.debug("RstTmpl: HandleError: {}", statusCode.value());
-      }
-    }).build();
-
+            })
+        .build();
   }
-
-
-
 }
